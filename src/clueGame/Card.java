18d64fe2e3e14d24b5542cb8d4c612d71f44a93c
JpWeim <jpwalter@mines.edu>
2020-11-01 17:38:10 -0700

    Missed this file in previous commit

--- src/clueGame/Card.java
+++ src/clueGame/Card.java
@@ -4,6 +4,10 @@ public class Card {
 	private String cardName;
 	private CardType type;
 	
+	public Card(String cardName, CardType type) {
+		this.cardName = cardName;
+		this.type = type;
+	}
 	public boolean equals(Card target) {
 		return true;
 	}
